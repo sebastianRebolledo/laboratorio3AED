36228ebb09e28bf2b8def163a177bec3e2aadc7f
sebastian <issareme@hotmail.com>
2019-04-26 00:38:00 -0500

    4#
    git statu
    git commit -m 4#

--- Lab3_Mercados_Financieros/src/test/TestBTree.java
+++ Lab3_Mercados_Financieros/src/test/TestBTree.java
@@ -4,11 +4,116 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import arbol_Avl.BNode;
+import arbol_Avl.BTree;
+import javafx.scene.Node;
+import modelo.Mercado;
+
 class TestBTree {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
+	BTree<Double, Mercado> treeAvl;
+	
+	public void scenOne() {
+		treeAvl= new BTree<>();
 	}
+	@Test
+	public void testInsert() {
+		scenOne();
+		Mercado marketOne= new Mercado("USB1", 25.0, "11/01/2019", "9:45");
+		Mercado marketTwo= new Mercado("USB2", 21.0, "12/01/2019", "8:45");
+		Mercado marketThree=new Mercado("USB3", 12.0, "10/01/2019", "8:35");
+		Mercado marketFour=new Mercado("USB4", 16.0, "09/01/2019", "10:35");
+		Mercado marketFive=new Mercado("USB5", 20.0, "01/01/2019", "3:35");
+treeAvl.insert(marketOne.getValor(), marketOne);
+treeAvl.insert(marketTwo.getValor(), marketTwo);
+treeAvl.insert(marketThree.getValor(), marketThree);
+treeAvl.insert(marketFour.getValor(), marketFour);
+treeAvl.insert(marketFive.getValor(), marketFive);
+
+int size=treeAvl.inorder_tree_walk2().size();
+assertEquals(size, 5);
+		
+				
+	}
+//	@Test
+//	public void testMaximium() {
+//		scenOne();
+//		MercadoInternacional marketOne= new MercadoInternacional("USB1", 18.0, "11/01/2019", "9:45");
+//		MercadoInternacional marketTwo= new MercadoInternacional("USB2", 21.0, "12/01/2019", "8:45");
+//		MercadoInternacional marketThree=new MercadoInternacional("USB3", 25.0, "10/01/2019", "8:35");
+//		MercadoInternacional marketFour=new MercadoInternacional("USB4", 16.0, "09/01/2019", "10:35");
+//		MercadoInternacional marketFive=new MercadoInternacional("USB5",15.0 , "01/01/2019", "3:35");
+//treeAvl.insert(marketOne.getValor(), marketOne);
+//treeAvl.insert(marketTwo.getValor(), marketTwo);
+//treeAvl.insert(marketThree.getValor(), marketThree);
+//treeAvl.insert(marketFour.getValor(), marketFour);
+//treeAvl.insert(marketFive.getValor(), marketFive);
+//BNode<Double, MercadoInternacional> nodeMaximum=treeAvl.maximum(treeAvl.getRoot());
+//assertEquals(nodeMaximum.getValue().getValor(),marketThree.getValor());
+//
+//
+//	}
+//	
+//	@Test
+//	public void testMinimium() {
+//		scenOne();
+//		MercadoInternacional marketOne= new MercadoInternacional("USB1", 18.0, "11/01/2019", "9:45");
+//		MercadoInternacional marketTwo= new MercadoInternacional("USB2", 21.0, "12/01/2019", "8:45");
+//		MercadoInternacional marketThree=new MercadoInternacional("USB3", 25.0, "10/01/2019", "8:35");
+//		MercadoInternacional marketFour=new MercadoInternacional("USB4", 16.0, "09/01/2019", "10:35");
+//		MercadoInternacional marketFive=new MercadoInternacional("USB5",15.0 , "01/01/2019", "3:35");
+//treeAvl.insert(marketOne.getValor(), marketOne);
+//treeAvl.insert(marketTwo.getValor(), marketTwo);
+//treeAvl.insert(marketThree.getValor(), marketThree);
+//treeAvl.insert(marketFour.getValor(), marketFour);
+//treeAvl.insert(marketFive.getValor(), marketFive);
+//BNode<Double, MercadoInternacional> nodeMinimum=treeAvl.minimum(treeAvl.getRoot());
+//assertEquals(nodeMinimum.getValue().getValor(),marketFive.getValor());
+//	
+//	}
+//	
+//	@Test
+//	public void testDelete() {
+//		scenOne();
+//		MercadoInternacional marketOne= new MercadoInternacional("USB1", 18.0, "11/01/2019", "9:45");
+//		MercadoInternacional marketTwo= new MercadoInternacional("USB2", 21.0, "12/01/2019", "8:45");
+//		MercadoInternacional marketThree=new MercadoInternacional("USB3", 25.0, "10/01/2019", "8:35");
+//		MercadoInternacional marketFour=new MercadoInternacional("USB4", 16.0, "09/01/2019", "10:35");
+//		MercadoInternacional marketFive=new MercadoInternacional("USB5",15.0 , "01/01/2019", "3:35");
+//treeAvl.insert(marketOne.getValor(), marketOne);
+//treeAvl.insert(marketTwo.getValor(), marketTwo);
+//treeAvl.insert(marketThree.getValor(), marketThree);
+//treeAvl.insert(marketFour.getValor(), marketFour);
+//treeAvl.insert(marketFive.getValor(), marketFive);
+//
+//treeAvl.delete( 25.0);
+//assertEquals(treeAvl.getRoot().getRight().getValue().getValor(), marketTwo.getValor());
+//assertEquals(treeAvl.inorder_tree_walk2().size(), 4);
+//
+//	}
+//	
+//	@Test
+//	public void testSearch() {
+//		scenOne();
+//		MercadoInternacional marketOne= new MercadoInternacional("USB1", 18.0, "11/01/2019", "9:45");
+//		MercadoInternacional marketTwo= new MercadoInternacional("USB2", 21.0, "12/01/2019", "8:45");
+//		MercadoInternacional marketThree=new MercadoInternacional("USB3", 25.0, "10/01/2019", "8:35");
+//		MercadoInternacional marketFour=new MercadoInternacional("USB4", 16.0, "09/01/2019", "10:35");
+//		MercadoInternacional marketFive=new MercadoInternacional("USB5",15.0 , "01/01/2019", "3:35");
+//treeAvl.insert(marketOne.getValor(), marketOne);
+//treeAvl.insert(marketTwo.getValor(), marketTwo);
+//treeAvl.insert(marketThree.getValor(), marketThree);
+//treeAvl.insert(marketFour.getValor(), marketFour);
+//treeAvl.insert(marketFive.getValor(), marketFive);
+//double keySearch=16.0;
+//	MercadoInternacional findMarket=treeAvl.search(keySearch);	
+//	assertEquals(findMarket.getValor(), keySearch);
+//	
+//	}
+	
+//	@Test
+//	void test() {
+//		fail("Not yet implemented");
+//	}
 
 }
