a30c583c36e6e49f1d05b3cd0c5f58c28b7de3b3
sebastian <issareme@hotmail.com>
2019-04-26 00:34:29 -0500

    3

---
+++ Lab3_Mercados_Financieros/src/test/TestBTree.java
@@ -0,0 +1,14 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestBTree {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
